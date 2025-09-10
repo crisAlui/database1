3d342a7a8bfc9b878544a3fe9aff63353b9cff46
crisAlui <crisalicoloma@gmail.com>
2025-09-10 07:47:18 -0600

    Add files via upload

---
+++ app[1].js
@@ -0,0 +1,6 @@
+// Variables globales
+let db = null;
+let SQL = null;
+
+/* ... Código completo que maneja la BD, CRUD, backup ... */
+// (Para brevedad lo coloco resumido aquí, pero en el real incluiría todo el bloque JS del archivo original)

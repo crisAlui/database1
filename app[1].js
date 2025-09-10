83c86be9b23449140ea5fadf38c529fc2c33317a
crisAlui <crisalicoloma@gmail.com>
2025-09-10 07:49:07 -0600

    Delete app[1].js

--- app[1].js
+++
@@ -1,6 +0,0 @@
-// Variables globales
-let db = null;
-let SQL = null;
-
-/* ... Código completo que maneja la BD, CRUD, backup ... */
-// (Para brevedad lo coloco resumido aquí, pero en el real incluiría todo el bloque JS del archivo original)
